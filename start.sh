8eff82aa2ccb2566a464aaa1550934db0a3b0bfa
Ayush64047 <bhaskarsharma64047@gmail.com>
2023-01-14 12:25:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,24 @@
-
-echo "Cloning Repo...."
-
-git clone https://https://github.com/Ayush64047/bot.git /bot
-
-cd /bot
-
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Ayush64047/bot.git /bot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /bot
+fi
+cd /TovinoBotV3
 pip3 install -U -r requirements.txt
-
-echo "⚡️⚡️ Starting bot..."
-
+echo "Starting bot...."
 python3 bot.py
+
+
+
+
+
+
+
+
+
+
+
 
