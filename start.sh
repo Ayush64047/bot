3581d1b7a1c2354cc96cd287e3a0a8c60ab71be7
Ayush64047 <bhaskarsharma64047@gmail.com>
2023-01-14 12:11:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,18 @@
-python3 bot.py
+
+echo "Cloning Repo...."
+
+git clone https://https://github.com/Ayush64047/bot.git /bot
+
+cd /bot
+
+pip3 install -U -r requirements.txt
+
+echo "⚡️⚡️ Starting bot..."
+
+python3 main.py
+
+Footer
+
+© 2023 GitHub, Inc.
+
+Footer navigation
